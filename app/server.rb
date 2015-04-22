ecc3c0fb7038a051fe8b5cdd690a52e0d6cec4ea
Joe Newman <jj_newman@hotmail.com>
2015-04-22 20:57:17 +0100

    Update server.rb

--- app/server.rb
+++ app/server.rb
@@ -6,44 +6,30 @@ require './lib/ship'
 require './lib/cell'
 require './lib/water'
 
-
 class Battleships < Sinatra::Base
 
   set :views, Proc.new {File.join(root, "..", "views")}
 
-
-  # board = Array.new(2) {Array.new(2) {'~'}
   board = Board.new(size: 3, content: Cell)
   ship = Ship.submarine
   game = Game.new
-  # cell = Cell.new
 
-
-
-
-   get '/' do
-    puts board.inspect
+  get '/' do
     erb :index
   end
-
 
   get '/name' do
     erb :name
   end
-
 
   post '/name' do
     if params[:name].empty?
       @message = 'please enter your name'
       erb :name
     else
-      # player = Player.new(params[:name])
-      # player.board
-      # game.add_player(player)
       redirect '/new_game'
     end
   end
-
 
   get '/new_game' do
     board.place(ship, :A1)
@@ -56,9 +42,5 @@ class Battleships < Sinatra::Base
     redirect '/new_game'
   end
 
-
-
-
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
